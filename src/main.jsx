b6dd4381e3b2928583e99463b9af2dea85bc8c44
Yoram <sebagyoram@gmail.com>
2026-05-11 23:01:47 +0300

    Remove PWA: manifest, service worker, icons, meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,3 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-// Unregister any existing service workers to prevent stale cache issues
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => {
-    regs.forEach(reg => reg.unregister())
-  })
-}
