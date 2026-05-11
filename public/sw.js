acad65b37f6754dd061e1bc00e0ef019dc9304b8
Yoram <sebagyoram@gmail.com>
2026-05-11 22:01:47 +0300

    Fix SW: skipWaiting + claim + purge old caches

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,22 @@
-self.addEventListener('install', e => e.waitUntil(
-  caches.open('padellink-v1').then(c => c.addAll(['/']))
-));
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-));
+const CACHE = 'padellink-v2';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match('/')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
